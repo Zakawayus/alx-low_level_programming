e3a955911df33fa88b70f299b79dfeac7f7c724c
Zakawayus <zakawayus@@gmail.com>
2021-09-09 12:46:46 +0100

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * *_strcat - concatenates two strings
+ * @dest: pointers destinations
+ * @src: pointers source
+ * Return: Always 0
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
